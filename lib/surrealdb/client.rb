0f29330726d314b626b0fb976770990f0a58e587
Takato Hano <oh48698062welcome@gmail.com>
2022-12-31 13:41:30 +0000

    implement SurrealdbClient

--- lib/surrealdb/client.rb
+++ lib/surrealdb/client.rb
@@ -5,6 +5,45 @@ require_relative "client/http"
 module Surrealdb
   # Client for surrealsb
   class Client
-    def initialize; end
+    # TODO: implement web_socket client
+    def initialize(url, namespace, database, username, password)
+      @client = Surrealdb::Client::Http.new(url, namespace, database, username, password)
+    end
+
+    def execute(query)
+      @client.execute(query)
+    end
+
+    def create_all(table, data)
+      @client.create_all(table, data)
+    end
+
+    def create_one(table, id, data)
+      @client.create_one(table, id, data)
+    end
+
+    def select_all(table)
+      @client.select_all(table)
+    end
+
+    def select_one(table, id)
+      @client.select_one(table, id)
+    end
+
+    def replace_one(table, id, data)
+      @client.replace_one(table, id, data)
+    end
+
+    def upsert_one(table, id, data)
+      @client.upsert_one(table, id, data)
+    end
+
+    def delete_all(table)
+      @client.delete_all(table)
+    end
+
+    def delete_one(table, id)
+      @client.delete_one(table, id)
+    end
   end
 end
